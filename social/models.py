d9139f4099284e27c2750b0fa926a5d5b86e9c24
Luchiwis <creppenderman@gmail.com>
2022-09-23 20:56:08 -0300

    hehcos los modelos y admin

--- social/models.py
+++ social/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models import CASCADE
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=CASCADE)
+    #TODO image
+
+    def __str__(self):
+        return f"perfil de {self.user.username}"
+    
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE, related_name="posts")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+    
+    def __str__(self):
+        return f"{self.user.username} : {self.content}"
